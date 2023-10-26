9818bb6c9c7a40763c9831c86b0158cc82d7744e
Flavio Barbosa <flaviombarbosa@gmail.com>
2023-10-26 11:48:03 -0300

    feat: frontend - get all books

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,5 @@
-insert into book(id, title, isbn10, pages, publisher, author, subject, published_at)
-values ('055f4232-aeb7-4ad2-9295-67ec7240c259', 'Clean Code', '8576082675', 240, 'Alta Books', 'Robert Martin', 'IT', '2000-10-23');
+insert into book(id, title, isbn10, isbn13, pages, publisher, author, subject, published_at)
+values ('055f4232-aeb7-4ad2-9295-67ec7240c259', 'Clean Code', '9780132350884', '978-0132350884', 431, 'Prentice Hall PTR', 'Robert Martin', 'IT', '2008-08-11');
+
+insert into book(id, title, isbn10, isbn13, pages, publisher, author, subject, published_at)
+values ('be85730f-da42-447b-96e4-7b36c99490c3', 'Clean Architecture', '0134494164', '978-0134494166', 432, 'Pearson', 'Robert Martin', 'IT', '2017-09-10');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,38 @@
-import './App.css'
-
+import { useQuery, gql } from '@apollo/client';
 function App() {
+
+  const GET_ALL_BOOKS = gql`
+    query getAllBooks {
+    books{
+        id
+        title
+        author
+        publisher
+        publishedAt
+        pages
+        isbn10
+        subject
+      }
+  }
+  `;
+
+  const {data, loading, error} = useQuery(GET_ALL_BOOKS);
+
+  if(loading) return <p>loading...</p>
+
+  if(error) return <p>Error: {error.message}</p>
 
   return (
     <>
-      <p>
-        Hello, Graphql
-      </p>
+    {
+      data.books.map(({id, title, pages, publisher, author, subject, publishedAt}) => (
+        <div key={id}>
+          <h3>{title}</h3>
+          <p>{author} - {pages} pages - {subject} - {publisher} - {publishedAt}</p>
+        </div>
+      ))
+    }
+
     </>
   )
 }
